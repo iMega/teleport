6f88b62e54ee1cf6b9b2222e99792b8e551ea7d5
Dmitry <irvis@imega.ru>
2015-06-20 11:54:49 +0300

    add sql teleport_enable

--- build/sql/teleport_enable.sql
+++ build/sql/teleport_enable.sql
@@ -1,3 +1,5 @@
-UPDATE `wp_options`
-  SET `option_name` = 'active_plugins', `option_value` = 'a:1:{i:0;s:33:\"imega-teleport/imega-teleport.php\";}', `autoload` = 'yes'
-  WHERE `option_id` = 33;
+use teleport;
+
+update `wp_options`
+  set `option_name` = 'active_plugins', `option_value` = 'a:1:{i:0;s:33:\"imega-teleport/imega-teleport.php\";}', `autoload` = 'yes'
+  where `option_id` = 33;
